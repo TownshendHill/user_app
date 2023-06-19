81a24c68d16ab3023638a2b6af847a9b2eaef35a
Max Tsai <a22222448@gmail.com>
2023-06-19 17:54:03 +0800

    Co-authored-by: terry <liaoiyang@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,58 @@
 <template>
-	<div id="app">
-		<p v-if="isLoading">Loading.....</p>
-		<div v-else>
-			<v-row>
-				<v-col cols="8">
-					<v-text-field
-						label="Number of users"
-						:rules="digitsRules"
-						v-model="numOfUsers"
-					></v-text-field>
-				</v-col>
-				<v-col cols="4">
-					<v-btn @click="onGetUsersClick">Get users</v-btn>
-				</v-col>
-			</v-row>
-			<div>num of users: {{ numOfUsers }}</div>
-			<UserCard
-				v-for="(user, index) in users"
-				:key="index"
-				:userInfo="user"
-				:isEven="index % 2 === 0"
-			></UserCard>
-		</div>
-	</div>
+	<v-app class="grey lighten-5">
+		<v-main>
+			<p v-if="isLoading">Loading.....</p>
+			<v-container v-else>
+				<v-row justify="space-around">
+					<v-col cols="8">
+						<v-text-field
+							label="Number of users"
+							:rules="digitsRules"
+							v-model="numOfUsers"
+						></v-text-field>
+					</v-col>
+					<v-col cols="4">
+						<v-btn @click="onGetUsersClick">Get users</v-btn>
+					</v-col>
+				</v-row>
+				<v-row>
+					<v-col>
+						<div>num of users: {{ numOfUsers }}</div>
+					</v-col>
+				</v-row>
+				<v-row>
+					<FilterOptions
+						:genders="genders"
+						:nations="nations"
+						@filterGender="filterGender"
+						@filterNation="filterNation"
+					></FilterOptions>
+				</v-row>
+				<v-row class="d-flex flex-wrap justify-space-between">
+					<UserCard
+						v-for="(user, index) in users"
+						:key="index"
+						:userInfo="user"
+						:isEven="index % 2 === 0"
+						:class="{ hide: hideDisplay }"
+					></UserCard>
+				</v-row>
+			</v-container>
+		</v-main>
+	</v-app>
 </template>
 <!-- comment -->
 
 <script>
 import { getUsers } from "@/api";
 import UserCard from "@/components/UserCard";
+import FilterOptions from "@/components/FilterOptions.vue";
 
 export default {
 	name: "App",
 	components: {
 		UserCard,
+		FilterOptions,
 	},
 	data() {
 		return {
@@ -41,7 +60,22 @@ export default {
 			isLoading: false,
 			numOfUsers: 5,
 			digitsRules: [(v) => /^\d+$/.test(v) || "Only digits are allowed"],
+			// genders: [],
+			// nations: [],
+			hideDisplay: false,
+			price: 100,
 		};
+	},
+	computed: {
+		discountPrice() {
+			return this.price / 2;
+		},
+		genders() {
+			return [...new Set(this.users.map((user) => user.gender))];
+		},
+		nations() {
+			return [...new Set(this.users.map((user) => user.nat))];
+		},
 	},
 	async created() {
 		console.log("App created");
@@ -49,6 +83,12 @@ export default {
 		try {
 			const response = await getUsers(this.numOfUsers);
 			this.users = response.data.results;
+
+			setTimeout(() => {
+				this.users = this.users.filter(
+					(user) => user.gender === "male"
+				);
+			}, 5000);
 		} catch (error) {
 			console.error("error", error);
 		} finally {
@@ -56,6 +96,13 @@ export default {
 		}
 	},
 	methods: {
+		filterGender(gender) {
+			console.log("gender click", gender);
+			this.hideDisplay = !this.hideDisplay;
+		},
+		filterNation(nation) {
+			console.log("nation click", nation);
+		},
 		async onGetUsersClick() {
 			try {
 				const response = await getUsers(this.numOfUsers);
@@ -66,6 +113,12 @@ export default {
 				this.isLoading = false;
 			}
 		},
+		// getGenders() {
+		// 	this.genders = [...new Set(this.users.map((user) => user.gender))];
+		// },
+		// getNations() {
+		// 	this.nations = [...new Set(this.users.map((user) => user.nat))];
+		// },
 	},
 	beforeUpdate() {
 		console.log("App beforeUpdate");
@@ -73,6 +126,9 @@ export default {
 	updated() {
 		console.log("App updated");
 	},
+	// beforeMount() {
+	// 	this.getGenders();
+	// },
 };
 </script>
 
@@ -84,5 +140,9 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+}
+
+#app .hide {
+	display: none;
 }
 </style>
---
+++ src/components/FilterOptions.vue
@@ -0,0 +1,81 @@
+<template>
+	<div style="width: 100%">
+		<v-row justify="space-around" v-show="genders.length > 1">
+			<v-col cols="12">
+				<!-- <v-chip pill class="blue lighten-5" @click="onFilterGender()"
+					>Male
+				</v-chip>
+				<v-chip pill class="pink lighten-5" @click="onFilterGender()"
+					>Female
+				</v-chip> -->
+				<v-chip
+					v-for="(gender, index) in genders"
+					:key="gender"
+					:class="{
+						'ml-4': index > 0,
+						'blue lighten-5': gender === 'male',
+						'pink lighten-5': gender === 'female',
+					}"
+					:genders="genders"
+					@click="onFilterGender(gender)"
+				>
+					{{ gender }}
+				</v-chip>
+			</v-col>
+		</v-row>
+		<v-row class="mt-0">
+			<v-col cols="12">
+				<v-chip
+					v-for="(nation, index) in nations"
+					:key="nation"
+					:class="{ 'ml-4': index > 0 }"
+					:nations="nations"
+					@click="onFilterNation(nation)"
+				>
+					<v-img
+						:src="getFlagImage"
+						loading
+						class="flex-grow-0 flex-shrink-1"
+						width="64"
+					/>
+					{{ nation }}
+				</v-chip>
+			</v-col>
+		</v-row>
+	</div>
+</template>
+<script>
+export default {
+	name: "FilterOptions",
+	props: {
+		genders: {
+			type: Array,
+			required: true,
+			default: () => {},
+		},
+		nations: {
+			type: Array,
+			required: true,
+		},
+	},
+	data() {
+		return {};
+	},
+	methods: {
+		onFilterGender(gender) {
+			this.$emit("filterGender", { gender, message: "test123" });
+		},
+		onFilterNation(nation) {
+			this.$emit("filterNation", nation);
+		},
+	},
+	computed: {
+		getFlagImage() {
+			return `https://flagsapi.com/${this.nation}/flat/32.png`;
+		},
+	},
+	mounted() {
+		console.log("can i use this?");
+	},
+};
+</script>
